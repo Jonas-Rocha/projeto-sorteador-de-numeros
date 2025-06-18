605f40e22c82d280ecaa69fea28de6763a3cb678
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-18 14:59:51 -0300

    fazendo o middle section sumir

--- script.js
+++ script.js
@@ -4,26 +4,20 @@ const numbers = document.getElementById("numbers");
 const numbersDe = document.getElementById("numbersDe");
 const numbersA = document.getElementById("numbersA");
 const toggle = document.getElementById("toggle");
+const middleSection = document.getElementById("middle-section");
 
 // Captura o valor digitado no input "NÚMEROS"
 numbers.addEventListener("input", () => {
   let valueNumbers = numbers.value.replace(/\D/g, "");
 
-  numbers.value = valueNumbers;
-
-  console.log(valueNumbers);
-
-  return valueNumbers;
+  numbers.value = Number(valueNumbers);
 });
 
 // Captura o valor digitado no input "DE"
 numbersDe.addEventListener("input", () => {
   let valueNumbersDe = numbersDe.value.replace(/\D/g, "");
 
-  valueNumbersDe = Number(valueNumbersDe);
-  console.log(valueNumbersDe);
-
-  return valueNumbersDe;
+  numbersDe.value = Number(valueNumbersDe);
 });
 
 // Captura o valor digitado no input "A"
@@ -31,8 +25,27 @@ numbersDe.addEventListener("input", () => {
 numbersA.addEventListener("input", () => {
   let valueNumbersA = numbersA.value.replace(/\D/g, "");
 
-  valueNumbersA = Number(valueNumbersA);
-  console.log(valueNumbersA);
+  numbersA.value = Number(valueNumbersA);
+});
 
-  return valueNumbersA;
+function numbersResult() {
+  const quantity = Number(numbers.value);
+  const minimum = Number(numbersDe.value);
+  const maximum = Number(numbersA.value);
+
+  const generatedNumbers = [];
+
+  for (let index = 0; index < quantity; index++) {
+    const random = Math.floor(Math.random() * (maximum - minimum)) + minimum;
+    generatedNumbers.push(random);
+  }
+
+  console.log(generatedNumbers);
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  numbersResult();
+
+  middleSection.setAttribute("style", "display: none;");
 });
