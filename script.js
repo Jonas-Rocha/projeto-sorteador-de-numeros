60ae7add455951db885a319f74dede4af862c541
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-18 12:20:48 -0300

    Capturando o valor dos inputs

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const form = document.querySelector("form");
+const submit = document.getElementsByClassName("submit");
+const numbers = document.getElementById("numbers");
+const numbersDe = document.getElementById("numbersDe");
+const numbersA = document.getElementById("numbersA");
+const toggle = document.getElementById("toggle");
 
+// Captura o valor digitado no input "NÚMEROS"
+numbers.addEventListener("input", () => {
+  let valueNumbers = numbers.value.replace(/\D/g, "");
+
+  valueNumbers = Number(valueNumbers);
+  console.log(valueNumbers);
+
+  return valueNumbers;
+});
+
+// Captura o valor digitado no input "DE"
+numbersDe.addEventListener("input", () => {
+  let valueNumbersDe = numbersDe.value.replace(/\D/g, "");
+
+  valueNumbersDe = Number(valueNumbersDe);
+  console.log(valueNumbersDe);
+
+  return valueNumbersDe;
+});
+
+// Captura o valor digitado no input "A"
+
+numbersA.addEventListener("input", () => {
+  let valueNumbersA = numbersA.value.replace(/\D/g, "");
+
+  valueNumbersA = Number(valueNumbersA);
+  console.log(valueNumbersA);
+
+  return valueNumbersA;
+});
